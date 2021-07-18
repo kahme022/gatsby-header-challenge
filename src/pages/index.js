723f2a0ebd5ff8cece9682322d1d3da9f8562ea5
kahme022 <kahme022@uottawa.ca>
2021-07-18 14:42:03 -0400

    gg

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import "../styles/styles.scss"
 
 const IndexPage = () => (
   <Layout>
